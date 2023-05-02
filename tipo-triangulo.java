3cc049dd310e2191cb8e8ce75ad5fcc30a887192
Kaua Barboza <132139127+KauaBarboza7@users.noreply.github.com>
2023-05-02 13:46:00 -0300

    Update tipo-triangulo.java

--- tipo-triangulo.java
+++ tipo-triangulo.java
@@ -1,36 +1,39 @@
 import java.util.Scanner;
 
 
- class TipoTriangulo {
+public class TipoTriangulo {
 	 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 		
 		int x, y, z;
+		double cateto1, cateto2, hipotenusa;
 		
 		
+		
+       		 System.out.print("Digite o valor do lado 1: ");
+         	x = scanner.nextInt();
 
-        System.out.print("Digite o valor do lado 1: ");
-         x = scanner.nextInt();
+        	System.out.print("Digite o valor do lado 2: ");
+        	 y = scanner.nextInt();
 
-        System.out.print("Digite o valor do lado 2: ");
-         y = scanner.nextInt();
-
-        System.out.print("Digite o valor do lado 3: ");
-         z = scanner.nextInt();
+        	System.out.print("Digite o valor do lado 3: ");
+         	z = scanner.nextInt();
+         
+        	System.out.print("\n\n");
 		 
 		 
-		 if(x > 0 && y > 0 && z > 0){
-			 
+		 if (x > 0 && y > 0 && z > 0){
 		 }
 		 
-		 else{
-		 System.out.printf("Este nao e um triangulo  "  \n\n);
-		 
+		 else {
+		System.out.printf(" Esta forma nao é a de um triangulo! " );
+		return;
 		 }
 		 
-         if( x == y && y == z){
-        System.out.printf("Este e um triangulo equilatero " );
+		 
+         	if( x == y && y == z){
+        	System.out.printf("Este e um triangulo equilatero " );
 		 }
 		 
 		 else if (x == y || x == z || y == z){
@@ -38,10 +41,24 @@ import java.util.Scanner;
 		 }
 		 
 		 else{
-	    System.out.printf("Este e um triangulo Escaleno " );	 
+	   	 System.out.printf("Este e um triangulo Escaleno " );
+	    
 		 } 
 		 
+	    	System.out.printf("\n\n\n" ); 
 		 
+	        System.out.print("Entre com o valor do cateto 1: ");
+                cateto1 = scanner.nextDouble();
+
+                System.out.print("Entre com o valor do cateto 2: ");
+                cateto2 = scanner.nextDouble();
+
+       		hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+        
+        	System.out.printf("\n\n" );
+
+       		System.out.println("A hipotenusa é: " + hipotenusa);
+      
 		  
     }
 }
